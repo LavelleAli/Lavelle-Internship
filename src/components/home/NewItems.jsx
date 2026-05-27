f6d6afa45a04973b77333c692cb668f5b541f5f1
Lavelle_Ali <lavelleali7@gmail.com>
2026-05-27 17:55:25 -0400

    rendered API data to page dynamically, created slider allowing users navigation throught NFT items

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,17 +1,25 @@
-import { useState, useEffect, useCallback } from "react";
-import { Link } from "react-router-dom";
+import { useState, useEffect, useCallback, useRef } from "react";
+import { Link, useParams } from "react-router-dom";
 import axios from "axios";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
 
 const NewItems = () => {
-
   const [newItemsData, setNewItemsData] = useState([]);
+  const sliderRef = useRef(null);
+  const { id } = useParams();
+
+
+
+
 
   const getNewItemsData = useCallback(async () => {
     try {
       const { data } = await axios.get(
-        "https://us-central1-nft-cloud-functions.cloudfunctions.net/newItems"
+        "https://us-central1-nft-cloud-functions.cloudfunctions.net/newItems",
       );
       setNewItemsData(data);
     } catch (error) {
@@ -23,9 +31,87 @@ const NewItems = () => {
     getNewItemsData();
   }, [getNewItemsData]);
 
-  const itemsToRender = newItemsData.length ? newItemsData.slice(0, 4) : new Array(4).fill(null);
+  const itemsToRender = newItemsData.length
+    ? newItemsData.slice(0, 4)
+    : new Array(4).fill(null);
 
+
+
+
+  function sliderSettings() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+    };
+
+    return settings;
+  }
   
+    
+
+  function dynamicRenderingOfNewItemsData(item, index) {
+    return (
+      
+        <div className="nft__item" key={index}>
+          <div className="author_list_pp">
+            <Link
+              to={`/author/${item.authorId}`}
+              data-bs-toggle="tooltip"
+              data-bs-placement="top"
+              title="Creator: Monica Lucas"
+            >
+              <img className="lazy" src={item.authorImage} alt="" />
+              <i className="fa fa-check"></i>
+            </Link>
+          </div>
+          <div className="de_countdown">5h 30m 32s</div>
+
+          <div className="nft__item_wrap">
+            <div className="nft__item_extra">
+              <div className="nft__item_buttons">
+                <button>Buy Now</button>
+                <div className="nft__item_share">
+                  <h4>Share</h4>
+                  <a href="" target="_blank" rel="noreferrer">
+                    <i className="fa fa-facebook fa-lg"></i>
+                  </a>
+                  <a href="" target="_blank" rel="noreferrer">
+                    <i className="fa fa-twitter fa-lg"></i>
+                  </a>
+                  <a href="">
+                    <i className="fa fa-envelope fa-lg"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <Link to={`/item-details/${item.nftId}`}>
+              <img
+                src={item.nftImage}
+                className="lazy nft__item_preview"
+                alt=""
+              />
+            </Link>
+          </div>
+          <div className="nft__item_info">
+            <Link to={`/item-details/${item.nftId}`}>
+              <h4>{item.title}</h4>
+            </Link>
+            <div className="nft__item_price">{item.price}ETH</div>
+            <div className="nft__item_like">
+              <i className="fa fa-heart"></i>
+              <span>{item.likes}</span>
+            </div>
+          </div>
+        </div>
+      
+    );
+  }
+
+
 
 
   return (
@@ -38,7 +124,18 @@ const NewItems = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {itemsToRender.map((_, index) => (
+          <div className="slider_container">
+          <Slider ref={sliderRef}
+           {...sliderSettings()}
+           className="newItems__Slider">
+
+            {newItemsData.map((items, index) => (
+              dynamicRenderingOfNewItemsData(items, index)
+            ))}
+
+          </Slider>
+          </div>
+          {/* {itemsToRender.map((_, index) => (
             <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
               <div className="nft__item">
                 <div className="author_list_pp">
@@ -93,7 +190,7 @@ const NewItems = () => {
                 </div>
               </div>
             </div>
-          ))}
+          ))} */}
         </div>
       </div>
     </section>
