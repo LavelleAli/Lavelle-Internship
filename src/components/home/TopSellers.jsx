c53067727522144d7538b7e1e7dafb86d99cc2ff
Lavelle_Ali <lavelleali7@gmail.com>
2026-05-31 07:22:54 -0400

    no changes made

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -25,7 +25,35 @@ useEffect(() => {
  { getTopsellersData()};
 }, [])
 
+function renderTopSellersData(item, id) {
+  return (
+    <div className="col-md-12">
+      <ol className="author_list">
+        {new Array(12).fill(0).map((_, index) => (
+          <li key={index}>
+            <div className="author_list_pp">
+              <Link to="/author">
+                <img
+                  className="lazy pp-author"
+                  src={AuthorImage}
+                  alt=""
+                />
+                <i className="fa fa-check"></i>
+              </Link>
+            </div>
+            <div className="author_list_info">
+              <Link to="/author">Monica Lucas</Link>
+              <span>2.1 ETH</span>
+            </div>
+          </li>
+        ))}
+      </ol>
+    </div>
 
+  )
+
+
+}
 
 
 
@@ -41,7 +69,11 @@ useEffect(() => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          <div className="col-md-12">
+          {/* {getTopsellersData.map((item, id) => renderTopSellersData())} */}
+
+
+
+          {/* <div className="col-md-12">
             <ol className="author_list">
               {new Array(12).fill(0).map((_, index) => (
                 <li key={index}>
@@ -62,7 +94,7 @@ useEffect(() => {
                 </li>
               ))}
             </ol>
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
