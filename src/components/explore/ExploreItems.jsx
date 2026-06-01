295a1b171be2c966430d0474f2b0605cbd49d111
Lavelle_Ali <lavelleali7@gmail.com>
2026-06-01 17:09:50 -0400

    Explore Items initial commit: installed axios & created functions to fetch API data and render data to page

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -1,9 +1,69 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect, useCallback } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
 
+
 const ExploreItems = () => {
+
+const [exploreItems, setExploreItems] = useState([]);
+
+const getExploreItems = useCallback( async () => {
+  try {
+    const { data } = await axios.get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/explore`);
+    setExploreItems(data);
+    console.log(data);
+  } catch (error) {
+    console.log("Error fetching Explore Items Data:", error);
+  }
+});
+
+useEffect(() => {
+  getExploreItems();
+}, []);
+
+
+function renderExploreItemsData(explore, id ) {
+  return (
+    <div
+          key={id}
+          className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+          style={{ display: "block", backgroundSize: "cover" }}
+         >
+          <div className="nft__item">
+            <div className="author_list_pp">
+              <Link
+                to="/author"
+                data-bs-toggle="tooltip"
+                data-bs-placement="top"
+              >
+                <img className="lazy" src={explore.authorImage} alt="" />
+                <i className="fa fa-check"></i>
+              </Link>
+            </div>
+            <div className="de_countdown">5h 30m 32s</div>
+
+            <div className="nft__item_wrap">
+              <Link to="/item-details">
+                <img src={explore.nftImage} className="lazy nft__item_preview" alt="" />
+              </Link>
+            </div>
+            <div className="nft__item_info">
+              <Link to="/item-details">
+                <h4>{explore.title}</h4>
+              </Link>
+              <div className="nft__item_price">{explore.price} ETH</div>
+              <div className="nft__item_like">
+                <i className="fa fa-heart"></i>
+                <span>{explore.likes}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+  )
+}
+
   return (
     <>
       <div>
@@ -14,60 +74,8 @@ const ExploreItems = () => {
           <option value="likes_high_to_low">Most liked</option>
         </select>
       </div>
-      {new Array(8).fill(0).map((_, index) => (
-        <div
-          key={index}
-          className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-          style={{ display: "block", backgroundSize: "cover" }}
-        >
-          <div className="nft__item">
-            <div className="author_list_pp">
-              <Link
-                to="/author"
-                data-bs-toggle="tooltip"
-                data-bs-placement="top"
-              >
-                <img className="lazy" src={AuthorImage} alt="" />
-                <i className="fa fa-check"></i>
-              </Link>
-            </div>
-            <div className="de_countdown">5h 30m 32s</div>
+      {exploreItems.slice(0, 8).map((explore, index) => renderExploreItemsData(explore, index)) }
 
-            <div className="nft__item_wrap">
-              <div className="nft__item_extra">
-                <div className="nft__item_buttons">
-                  <button>Buy Now</button>
-                  <div className="nft__item_share">
-                    <h4>Share</h4>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-facebook fa-lg"></i>
-                    </a>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-twitter fa-lg"></i>
-                    </a>
-                    <a href="">
-                      <i className="fa fa-envelope fa-lg"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <Link to="/item-details">
-                <img src={nftImage} className="lazy nft__item_preview" alt="" />
-              </Link>
-            </div>
-            <div className="nft__item_info">
-              <Link to="/item-details">
-                <h4>Pinky Ocean</h4>
-              </Link>
-              <div className="nft__item_price">1.74 ETH</div>
-              <div className="nft__item_like">
-                <i className="fa fa-heart"></i>
-                <span>69</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      ))}
       <div className="col-md-12 text-center">
         <Link to="" id="loadmore" className="btn-main lead">
           Load more
--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,8 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useCallback } from "react";
+import {Link, useParams} from "react-router-dom";
+import axios from "axios";
 import SubHeader from "../images/subheader.jpg";
 import ExploreItems from "../components/explore/ExploreItems";
 
 const Explore = () => {
+
+
+
+function renderExploreItemsData() {
+  return (
+     <section aria-label="section">
+          <div className="container">
+            <div className="row">
+              <ExploreItems />
+            </div>
+          </div>
+        </section>
+        );
+}
+
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
