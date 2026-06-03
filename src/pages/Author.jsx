d4d1757b324cab00fc1eeee4e0fc87ccd1db8333
Lavelle_Ali <lavelleali7@gmail.com>
2026-06-03 14:45:03 -0400

    Author update: Created function to render skeleton cards to page

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,28 +1,25 @@
-import { useState, useEffect, useCallback} from "react";
+import { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 import { Link, useParams } from "react-router-dom";
 import AuthorBanner from "../images/author_banner.jpg";
 import AuthorItems from "../components/author/AuthorItems";
-import AuthorImage from "../images/author_thumbnail.jpg";
-
 
 const Author = (item) => {
   const { id } = useParams();
-  const [authorData, setAuthorData] = useState();
+  const [authorData, setAuthorData] = useState(null);
   const [loading, setLoading] = useState(true);
-
 
 
   const fetchAuthorData = useCallback(async () => {
     try {
-      const { data } = await axios.get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${id}`);
+      const { data } = await axios.get(
+        `https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=73855012`,
+      );
       setAuthorData(data);
       console.log(data);
-    } 
-    catch (error) {
+    } catch (error) {
       console.error("Error fetching author data:", error);
-    }
-    finally {
+    } finally {
       setLoading(false);
     }
   }, [id]);
@@ -31,67 +28,95 @@ const Author = (item) => {
     fetchAuthorData();
   }, [fetchAuthorData]);
 
+  function renderAuthorItems(item, id) {
+    return (
+      <div className="col-md-12" key={id}>
+        <div className="d_profile de-flex">
+          <div className="de-flex-col">
+            <div className="profile_avatar">
+              <img src={item?.authorImage} alt="" />
 
-  // function renderAuthorItems(item, id) {
-  //   return (
-     
-  //   );
-  // }
+              <i className="fa fa-check"></i>
+              <div className="profile_name">
+                <h4>
+                  {item?.authorName}
+                  <span className="profile_username">@{item.authorName}</span>
+                  <span id="wallet" className="profile_wallet">
+                    {item.address}
+                  </span>
+                  <button id="btn_copy" title="Copy Text">
+                    Copy
+                  </button>
+                </h4>
+              </div>
+            </div>
+          </div>
+          <div className="profile_follow de-flex">
+            <div className="de-flex-col">
+              <div className="profile_follower">{item.followers} followers</div>
+              <Link to="#" className="btn-main">
+                Follow
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
-
+  function skeletonLoader() {
+    return (
+      <div className="col-md-12">
+        <div className="d_profile de-flex">
+          <div className="de-flex-col">
+            <div className="profile_avatar">
+              <div className="profile_name">
+                <h4>
+                  <span className="profile_username"></span>
+                  <span id="wallet" className="profile_wallet"></span>
+                  <button id="btn_copy" title="Copy Text"></button>
+                </h4>
+              </div>
+            </div>
+          </div>
+          <div className="profile_follow de-flex">
+            <div className="de-flex-col">
+              <div className="profile_follower"></div>
+              <Link to="#" className="btn-main"></Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
         <div id="top"></div>
 
-
         <section
           id="profile_banner"
           aria-label="section"
           className="text-light"
           data-bgimage="url(images/author_banner.jpg) top"
-          style={{ background: `url(${AuthorBanner}) top` }}
+          style={{ background: `url(${authorData?.authorImage}) top` }}
         ></section>
 
         <section aria-label="section">
           <div className="container">
             <div className="row">
-              <div className="col-md-12">
-                <div className="d_profile de-flex">
-                  <div className="de-flex-col">
-                    <div className="profile_avatar">
-                      <img src={item.authorImage} alt="" />
-
-                      <i className="fa fa-check"></i>
-                      <div className="profile_name">
-                        <h4>
-                          Monica Lucas
-                          <span className="profile_username">@monicaaaa</span>
-                          <span id="wallet" className="profile_wallet">
-                            UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
-                          </span>
-                          <button id="btn_copy" title="Copy Text">
-                            Copy
-                          </button>
-                        </h4>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="profile_follow de-flex">
-                    <div className="de-flex-col">
-                      <div className="profile_follower">573 followers</div>
-                      <Link to="#" className="btn-main">
-                        Follow
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              {loading 
+              ? skeletonLoader()
+              : authorData && renderAuthorItems(authorData)}
 
               <div className="col-md-12">
                 <div className="de_tab tab_simple">
-                  <AuthorItems />
+                  <AuthorItems
+                    nftCollection={authorData?.nftCollection}
+                    authorImage={authorData?.authorImage}
+                    authorId={authorData?.authorId}
+                  />
                 </div>
               </div>
             </div>
