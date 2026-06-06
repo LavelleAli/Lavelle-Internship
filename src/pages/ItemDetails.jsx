d9574843f3a075eae38a3055ac7f3472bbbed29e
Lavelle_Ali <lavelleali7@gmail.com>
2026-06-06 12:56:09 -0400

    Fixed bugs

--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -15,7 +15,6 @@ const ItemDetails = () => {
         `https://us-central1-nft-cloud-functions.cloudfunctions.net/itemDetails?nftId=${id}`,
       );
       setItemDetails(data);
-      console.log(data)
     } catch (error) {
       console.log("Error fetching API data", error);
     } finally {
